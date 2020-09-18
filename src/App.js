f5bfcbf49b6a02747d6378410054d2599bf5388d
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-09-18 22:41:21 +0800

    banner component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Banner from './Components/Banner/Banner';
-import Header from './Components/Header/Header';
+import News from './Components/News/News';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact';
+import NoMatch from './Components/NoMatch/NoMatch';
 
 function App() {
   return (
-    <div>
-      <Banner></Banner>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/home'>
+          <Banner></Banner>
+        </Route>
+        <Route path='/news'>
+          <News></News>
+        </Route>
+        <Route path='/blog'>
+          <Blog></Blog>
+        </Route>
+        <Route path='/contact'>
+          <Contact></Contact>
+        </Route>
+        <Route exact path='/'>
+          <Banner></Banner>
+        </Route>
+        <Route path='*'>
+          <NoMatch></NoMatch>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Navbar, Nav, Form, FormControl} from 'react-bootstrap';
 import './Header.css';
 import logo from '../../images/Logo.png';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -13,13 +14,12 @@ function Header() {
                     <FormControl type="text" placeholder="Search your destination"  style={{width: "320px"}}/>
                 </Form>
                 <Nav className="mr-auto">
-                    <Nav.Link to ="/" className="nav_list" id="news">News</Nav.Link>
-                    <Nav.Link to ="/" className="text-white">Destination</Nav.Link>
-                    <Nav.Link to ="/" className="text-white">Blog</Nav.Link>
-                    <Nav.Link to ="/" className="text-white">Contact</Nav.Link>
+                    <Nav.Link><Link to='/news' className="nav_list" id="news">News</Link></Nav.Link>
+                    <Nav.Link><Link to='/destination' className="text-white">Destination</Link></Nav.Link>
+                    <Nav.Link><Link to='blog' className="text-white">Blog</Link></Nav.Link>
+                    <Nav.Link><Link to='contact' className="text-white">Contact</Link></Nav.Link>
                     <button className='btn'  style={{background: "orange", padding: "10px 30px", borderRadius: "7px"}}>Login</button>
                 </Nav>
-                
             </Navbar>
         </div>
     );
